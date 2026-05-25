405bc7d54d5b91381bbd2bd764ba9c859c2cd500
buildtheshow <buildtheshowapp@gmail.com>
2026-05-25 15:47:14 -0700

    Fetch registration page directly

--- functions/[org]/[show]/registration/[code].js
+++ functions/[org]/[show]/registration/[code].js
@@ -6,7 +6,7 @@ export async function onRequest(context) {
   const url = new URL(context.request.url);
   const assetUrl = new URL('/SYSTEM/Public/registration', url);
   try {
-    return await fetch(new Request(assetUrl.toString(), context.request));
+    return await fetch(assetUrl.toString());
   } catch (error) {
     return new Response('Registration form is temporarily unavailable.', {
       status: 503,
