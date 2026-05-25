cfe5c9f51ec56ea8e294d0e7ed13fa1f88db9ec8
buildtheshow <buildtheshowapp@gmail.com>
2026-05-25 15:43:14 -0700

    Fix public registration route asset fetch

--- functions/[org]/[show]/registration/[code].js
+++ functions/[org]/[show]/registration/[code].js
@@ -5,5 +5,12 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
   const assetUrl = new URL('/SYSTEM/Public/registration.html', url);
-  return context.env.ASSETS.fetch(assetUrl);
+  try {
+    return await context.env.ASSETS.fetch(new Request(assetUrl.toString(), context.request));
+  } catch (error) {
+    return new Response('Registration form is temporarily unavailable.', {
+      status: 503,
+      headers: { 'content-type': 'text/plain; charset=utf-8' },
+    });
+  }
 }
