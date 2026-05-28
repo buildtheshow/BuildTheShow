b948add6d2c83307e304c5cdb6623c71de0f99ac
buildtheshow <buildtheshowapp@gmail.com>
2026-05-28 15:42:40 -0700

    Fix volunteer quiz clean route

---
+++ functions/volunteer-quiz.js
@@ -0,0 +1,8 @@
+// Cloudflare Pages Function
+// Routes /volunteer-quiz -> standalone volunteer quiz page.
+
+export async function onRequest(context) {
+  const url = new URL(context.request.url);
+  const assetUrl = new URL('/volunteer-quiz/', url);
+  return context.env.ASSETS.fetch(new Request(assetUrl.toString(), context.request));
+}
