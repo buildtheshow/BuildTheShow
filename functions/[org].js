1ad8bb3a386d75655a8c2eccd43342ae2c34c9d2
buildtheshow <buildtheshowapp@gmail.com>
2026-05-29 12:18:23 -0700

    Fix /volunteers redirect loop: move to RESERVED_ROOTS, remove conflicting _redirects rule

--- functions/[org].js
+++ functions/[org].js
@@ -11,6 +11,7 @@ const RESERVED_ROOTS = new Set([
   'SYSTEM',
   'functions',
   'supabase',
+  'volunteers',
 ]);
 
 const CLEAN_ROUTE_ASSETS = new Map([
@@ -20,7 +21,6 @@ const CLEAN_ROUTE_ASSETS = new Map([
   ['reset-password', '/SYSTEM/Members/Account/reset-password.html'],
   ['find', '/PUBLIC/find.html'],
   ['volunteer-quiz', '/PUBLIC/volunteer-quiz'],
-  ['volunteers', '/volunteers/index.html'],
   ['opportunity', '/PUBLIC/opportunity-detail.html'],
   ['invite', '/PUBLIC/accept-invite.html'],
   ['audition', '/SYSTEM/Public/audition.html'],
